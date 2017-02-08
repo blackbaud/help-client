e405b184aa38aed8ea1909087fd6660707727029
Blackbaud Sky Build User <sky-build-user@blackbaud.com>
2017-02-08 11:44:25 -0500

    Added styles to accommodate pairing with omnibar

--- index.ts
+++ index.ts
@@ -11,9 +11,23 @@ export class BBHelp {
     });
   }
 
+  public static addStyles(): void {
+    let css = `
+      .bb-omnibar-bar.bar { padding-right: 50px; }
+      .bb-omnibar > .bb-omnibar-desktop > .bb-omnibar-accountflyout { right: 50px !important; }
+    `;
+    let style = document.createElement('style');
+    style.type = 'text/css';
+    style.appendChild(document.createTextNode(css));
+    document.head.appendChild(style);
+  }
+
   public static load(config: any = {}): Promise<any> {
     return BBHelp.registerScript('https://cdn.blackbaudcloud.com/bb-help/bb-help.js')
       .then(() => {
+        BBHelp.addStyles();
+
+        // Initialize the widget.
         BBHELP.HelpWidget.load(config);
       });
   }
