b7574d3876fc9b66424f68c8d09eb2f7d25bfbae
bdjones2410 <brandon.jones@blackbaud.com>
2018-03-14 09:43:43 -0400

    added a ready check

--- src/help.ts
+++ src/help.ts
@@ -5,6 +5,7 @@ import { registerScript } from './register-script';
 export abstract class BBHelpClient {
   private static defaultHelpKey: string = 'default.html';
   private static currentHelpKey: string;
+  private static widgetLoaded: boolean = false;
 
   public static addStyles(): void {
     const css = `
@@ -30,6 +31,7 @@ export abstract class BBHelpClient {
         BBHelpClient.addStyles();
         // Initialize the widget.
         BBHELP.HelpWidget.load(config);
+        BBHelpClient.widgetLoaded = true;
       });
   }
 
@@ -42,7 +44,10 @@ export abstract class BBHelpClient {
   }
 
   public static openWidgetToHelpKey(helpKey: string = BBHelpClient.currentHelpKey): void {
-    BBHELP.HelpWidget.open(helpKey);
+    BBHelpClient.executeWhenReady(
+      () => {
+        BBHELP.HelpWidget.open(helpKey);
+      });
   }
 
   public static getCurrentHelpKey(): string {
@@ -50,22 +55,54 @@ export abstract class BBHelpClient {
   }
 
   public static toggleOpen(): void {
-    BBHELP.HelpWidget.toggleOpen();
+    BBHelpClient.executeWhenReady(BBHELP.HelpWidget.toggleOpen);
   }
 
   public static openWidget(): void {
-    BBHELP.HelpWidget.open();
+    BBHelpClient.executeWhenReady(BBHELP.HelpWidget.open);
   }
 
   public static closeWidget(): void {
-    BBHELP.HelpWidget.close();
+    BBHelpClient.executeWhenReady(BBHELP.HelpWidget.close);
   }
 
   public static disableWidget(): void {
-    BBHELP.HelpWidget.disableWidget();
+    BBHelpClient.executeWhenReady(BBHELP.HelpWidget.disableWidget);
   }
 
   public static enableWidget(): void {
-    BBHELP.HelpWidget.enableWidget();
+    BBHelpClient.executeWhenReady(BBHELP.HelpWidget.enableWidget);
+  }
+
+  private static executeWhenReady(actionCallback: Function) {
+    BBHelpClient.ready()
+      .then(() => {
+        actionCallback();
+      })
+      .catch(err => {
+        console.error(err);
+      });
+  }
+
+  private static ready() {
+    return new Promise((resolve, reject) => {
+      let readyAttempts: number = 0;
+      const duration: number = 100;
+      const maxIterations: number = 100;
+
+      const interval = setInterval(() => {
+        readyAttempts++;
+
+        if (BBHelpClient.widgetLoaded) {
+          clearInterval(interval);
+          return resolve();
+        }
+
+        if (maxIterations >= 100) {
+          clearInterval(interval);
+          return reject('The Help Widget failed to load.');
+        }
+      }, duration);
+    });
   }
 }
