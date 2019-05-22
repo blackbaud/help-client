ee7081defb3572f26e9c5fe286b8133f5d832024
Tim Pepper <38473653+Blackbaud-TimPepper@users.noreply.github.com>
2019-05-22 10:21:57 -0400

    create amd bundle for RE and FE use case (#42)
    * create amd bundle for RE and FE use case
    
    * create aggregate compress script
    
    * remove changes from dev server

--- config/webpack/webpack.prod.config.js
+++ config/webpack/webpack.prod.config.js
@@ -1,32 +1,39 @@
 var path = require('path');
 
-module.exports = {
-  entry: './index.ts',
-  output: {
-    path: path.resolve(__dirname, '..', '..', 'dist', 'bundles'),
-    filename: 'help-client.umd.js',
-    library: 'BBHelpClient',
-    libraryTarget: 'umd'
-  },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  module: {
-    rules: [
-      {
-        test: /\.ts$/,
-        loader: 'ts-loader',
-        options: {
-          compilerOptions: {
-            declaration: false
+function createConfig(target) {
+  return {
+    entry: './index.ts',
+    output: {
+      path: path.resolve(__dirname, '..', '..', 'dist', 'bundles'),
+      filename: `help-client.${target}.js`,
+      library: 'BBHelpClient',
+      libraryTarget: target
+    },
+    resolve: {
+      extensions: ['.ts', '.js']
+    },
+    module: {
+      rules: [
+        {
+          test: /\.ts$/,
+          loader: 'ts-loader',
+          options: {
+            compilerOptions: {
+              declaration: false
+            }
           }
-        }
-      },
-      {
-        test: /\.scss$/,
-        loaders: ['style-loader', 'css-loader', 'sass-loader'],
-        exclude: '/node_modules/'
-      },
-    ]
+        },
+        {
+          test: /\.scss$/,
+          loaders: ['style-loader', 'css-loader', 'sass-loader'],
+          exclude: '/node_modules/'
+        },
+      ]
+    }
   }
 };
+
+module.exports = [
+  createConfig('umd'),
+  createConfig('amd')
+];
