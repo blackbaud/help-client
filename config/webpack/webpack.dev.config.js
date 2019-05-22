3f9baf42c301340e7ca21dcd535de41f660a987e
Blackbaud-TimPepper <tim.pepper@blackbaud.com>
2019-05-22 09:15:39 -0400

    remove changes from dev server

--- config/webpack/webpack.dev.config.js
+++ config/webpack/webpack.dev.config.js
@@ -2,54 +2,47 @@ var path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const fs = require('fs-extra');
 
-function createConfig(target) {
-  return {
-    entry: './index.ts',
-    output: {
-      path: path.resolve(__dirname, '..', '..', 'dist', 'bundles'),
-      filename: `help-client.${target}.js`,
-      library: 'BBHelpClient',
-      libraryTarget: target
-    },
-    devtool: 'inline-source-map',
-    resolve: {
-      extensions: ['.ts', '.js']
-    },
-    module: {
-      rules: [
-        {
-          test: /\.ts$/,
-          loader: 'ts-loader'
-        },
-        {
-          test: /\.scss$/,
-          loaders: ['style-loader', 'css-loader', 'sass-loader'],
-          exclude: '/node_modules/'
-        },
-      ]
-    },
-    devServer: {
-      headers: {
-        'Access-Control-Allow-Origin': '*'
+module.exports = {
+  entry: './index.ts',
+  output: {
+    path: path.resolve(__dirname, '..', '..', 'dist', 'bundles'),
+    filename: 'help-client.umd.js',
+    library: 'BBHelpClient',
+    libraryTarget: 'umd'
+  },
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: 'ts-loader'
       },
-      contentBase: '/dist/bundles',
-      port: 9000,
-      https: {
-        key: fs.readFileSync(path.resolve(__dirname, '..', 'cert', 'server.key')),
-        cert: fs.readFileSync(path.resolve(__dirname, '..', 'cert', 'server.crt'))
-      }
-    },
-    plugins: [
-      new HtmlWebpackPlugin({
-        template: 'template.html',
-        filename: 'index.html',
-        devServer: 'http://localhost:9000'
-      })
+      {
+        test: /\.scss$/,
+        loaders: ['style-loader', 'css-loader', 'sass-loader'],
+        exclude: '/node_modules/'
+      },
     ]
-  }
+  },
+  devServer: {
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    contentBase: '/dist/bundles',
+    port: 9000,
+    https: {
+      key: fs.readFileSync(path.resolve(__dirname, '..', 'cert', 'server.key')),
+      cert: fs.readFileSync(path.resolve(__dirname, '..', 'cert', 'server.crt'))
+    }
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'template.html',
+      filename: 'index.html',
+      devServer: 'http://localhost:9000'
+    })
+  ]
 };
-
-module.exports = [
-  createConfig('umd'),
-  createConfig('amd')
-];
