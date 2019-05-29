8cbea99717337af326bef4a8c623d737dcbfb4df
Blackbaud-TimPepper <tim.pepper@blackbaud.com>
2019-05-29 13:22:33 -0400

    target window for RE CDN fix

---
+++ src/bb-help.ts
@@ -0,0 +1,20 @@
+import { BBHelpHelpWidget } from './help-widget';
+import { BBHelpHelpWidgetRenderer } from './help-widget-renderer';
+import { BBHelpStyleUtility } from './help-widget-style-utility';
+import { BBHelpAnalyticsService } from './service/analytics.service';
+import { BBHelpCommunicationService } from './service/communication.service';
+import { MixpanelKeys } from './service/mixpanel-keys';
+
+const styleUtility = new BBHelpStyleUtility();
+const widgetRenderer = new BBHelpHelpWidgetRenderer(styleUtility);
+const mixpanelKeys = new MixpanelKeys();
+const analyticsService = new BBHelpAnalyticsService(mixpanelKeys);
+const communicationService = new BBHelpCommunicationService();
+
+const helpWidget = new BBHelpHelpWidget(
+  widgetRenderer,
+  analyticsService,
+  communicationService
+);
+
+export { helpWidget as HelpWidget };
