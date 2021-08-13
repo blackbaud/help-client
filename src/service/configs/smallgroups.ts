d8924e5c5054de5b958839d9d645b573b5090689
Colby M. White <blackbaud-colbywhite@users.noreply.github.com>
2021-08-13 11:14:53 -0500

    feat: move config merge logic from bb-help spa

---
+++ src/service/configs/smallgroups.ts
@@ -0,0 +1,7 @@
+import { HelpConfig } from '../../help-config';
+
+export const config: HelpConfig = {
+  productId: 'smallgroups',
+  useFlareSearch: true,
+  defaultHelpKey: 'smallgroups.html'
+};
