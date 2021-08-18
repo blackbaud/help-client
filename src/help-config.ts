962877f0fa553f40d22a3464dbcf7a5e57090bff
Colby M. White <blackbaud-colbywhite@users.noreply.github.com>
2021-08-18 10:09:20 -0500

    doc: better deprecation comments

--- src/help-config.ts
+++ src/help-config.ts
@@ -1,9 +1,13 @@
 export interface HelpConfig {
   /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
    * @deprecated
    */
   caseCentralUrl?: string;
   /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
    * @deprecated
    */
   communityUrl?: string;
@@ -21,14 +25,28 @@ export interface HelpConfig {
   environmentId?: string;
   extends?: string;
   /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
    * @deprecated
    */
   getChatData?: any;
   getCurrentHelpKey?: any;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
   headerColor?: string;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
   headerTextColor?: string;
   helpBaseUrl?: string | ((locale: string) => string);
   /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
    * @deprecated
    */
   helpCenterUrl?: string;
@@ -38,38 +56,23 @@ export interface HelpConfig {
   hideHelpChat?: boolean | string;
   helpUpdateCallback?: (args: { url: string }) => void;
   /**
+   * The undock component no longer exists, thus this configuration will have no effect.
    * @deprecated
    */
   hideUndock?: boolean | string;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
   hideWidgetOnMobile?: boolean;
   /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
    * @deprecated
    */
   knowledgebaseUrl?: string;
   locale?: string;
-  onHelpLoaded?: any;
-  productId?: string;
-  /**
-   * @deprecated
-   */
-  searchService?: string;
-  /**
-   * @deprecated
-   */
-  trainingCentralUrl?: string;
-  /**
-   * @deprecated
-   */
-  useFlareSearch?: boolean;
-  /**
-   * @deprecated
-   */
-  whatsNewRevisions?: any;
-  hostQueryParams?: string;
-  /**
-   * @deprecated
-   */
-  whats_new_help_key?: string;
   /**
    * Determines if widget will attempt to mimic being in the omnibar.
    * If true (default), the widget will create a green-ear trigger in the DOM that is designed to be on top of the omnibar.
@@ -77,4 +80,35 @@ export interface HelpConfig {
    * It's recommended to disable omnibar mimicking.
    */
   mimicOmnibar?: boolean;
+  onHelpLoaded?: any;
+  productId?: string;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
+  searchService?: string;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
+  trainingCentralUrl?: string;
+  /**
+   * This does nothing when {@link HelpConfig#mimicOmnibar} is false.
+   * Instead of using this property, disable omnibar mimicking.
+   * @deprecated
+   */
+  useFlareSearch?: boolean;
+  /**
+   * This was a proposed solution to What's new years ago that never was acted upon.
+   * @deprecated
+   */
+  whatsNewRevisions?: any;
+  hostQueryParams?: string;
+  /**
+   * This was a proposed solution to What's new years ago that never was acted upon.
+   * @deprecated
+   */
+  whats_new_help_key?: string;
 }
